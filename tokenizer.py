a5a8cab42475963705cf3cb905fbe912a3e345d2
Hope Kagame <kagame458@protonmail.com>
2022-03-14 13:58:30 +0300

    Add error logs Add condition regex Add logical operation regex

--- tokenizer.py
+++ tokenizer.py
@@ -14,9 +14,11 @@ class Token(NamedTuple):
 code = open("code.txt", "r").read()
 
 def tokenize(code):
-    keywords = {'using', 'namespace', 'std', 'float',
-                'string', 'cout', 'cin', '<<', '>>', 'if', 'else', 'return'}
+    keywords = {'using', 'namespace', 'std', 'float', 'int',
+                'string', 'cout', 'cin', 'if', '<<', '>>', 'else', 'return'}
     token_specification = [
+        
+        ('LOGICAL',             r'&& | \|\|'),                         # LOGICAL OPERATOR
         ('COMMENT',             r'//+(.*)'),                           # COMMENT
         ('STD_OUT',             r'<<'),                                # STDOUT SYMBOL
         ('STD_IN',              r'>>'),                                # STDIN SYMBOL
@@ -31,38 +33,45 @@ def tokenize(code):
         ('ASSIGN',              r'='),                                 # Assignment operator
         ('END',                 r';'),                                 # Statement terminator
         ('IDENTIFIERS',         r'[A-Za-z][A-Za-z_]+'),                # Identifiers
-        ('OP',                  r'[+\-*/]'),                           # Arithmetic operators
+        ('OP',                  r'[+\-*/><]'),                         # Arithmetic operators
         ('NEWLINE',             r'\n'),                                # Line endings
         ('SKIP',                r'[ \t]+'),                            # Skip over spaces and tabs
-        # ('MISMATCH',      r'.'),                                     # Any other character
+        ('MISMATCH',            r'.'),                                 # Any other character
+        # ('CONDITION',           r'if \(([A-Za-z><]*)\)'),              # CONDITIONS
     ]
     tok_regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
     line_num = 1
     line_start = 0
-    for matched_object in re.finditer(tok_regex, code):
-        kind = matched_object.lastgroup
-        value = matched_object.group()
-        column = matched_object.start() - line_start
-        if kind == 'NUMBER':
-            value = float(value) if '.' in value else int(value)
-        elif kind == 'IDENTIFIERS' and value in keywords:
-            kind = value
-        elif kind == 'INCLUDE':
-            kind = 'include'
-        elif kind == 'STRING':
-            kind = 'String'
-        elif kind == 'FUNCTION':
-            kind = 'Function'
-        elif kind == 'NEWLINE':
-            line_start = matched_object.end()
-            line_num += 1
-            continue
-        elif kind == 'SKIP':
-            continue
-        elif kind == 'MISMATCH':
-            raise RuntimeError(f'{value!r} unexpected on line {line_num}')
-        yield Token(kind, value, line_num, column)
 
+    with open('errors.txt', 'w') as file:
+        error_messages = ''
+        for matched_object in re.finditer(tok_regex, code):
+            kind = matched_object.lastgroup
+            value = matched_object.group()
+            column = matched_object.start() - line_start
+            if kind == 'NUMBER':
+                value = float(value) if '.' in value else int(value)
+            elif kind == 'IDENTIFIERS' and value in keywords:
+                kind = value
+            elif kind == 'INCLUDE':
+                kind = 'include'
+            elif kind == 'STRINGLITERAL':
+                kind = 'String'
+            elif kind == 'FUNCTION':
+                kind = 'Function'
+            elif kind == 'CONDITION':
+                kind = 'Condition'
+            elif kind == 'NEWLINE':
+                line_start = matched_object.end()
+                line_num += 1
+                continue
+            elif kind == 'SKIP':
+                continue
+            elif kind == 'MISMATCH':
+                # raise RuntimeError(f'{value!r} unexpected on line {line_num}')
+                error_messages += f'{value!r} unexpected on line {line_num}' + '\n'
+            yield Token(kind, value, line_num, column)
+        file.write(error_messages)
 
 codenot = r'''
 #include <iostream>
